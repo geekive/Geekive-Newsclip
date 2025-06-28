ea8073e6d67f62b107234233528dc51ad4e99617
geekive <geekive.com@gmail.com>
2025-06-28 23:55:21 +0900

    Initail commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")  # templates/index.html을 렌더링
+
+if __name__ == "__main__":
+    app.run(debug=True)
